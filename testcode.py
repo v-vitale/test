6e99dd6ecce15e475dd5dd1b2ba6bf5aa61e5b18
Vittorio Vitale <vvitale@sissa.it>
2019-07-02 10:09:55 +0200

    first code function

---
+++ testcode.py
@@ -0,0 +1,22 @@
+import numpy as np
+import scipy as sc
+import quitp as qt
+
+
+def purity(rho):
+    """
+    Calculate the purity of a quantum state
+    
+    Parameters
+    ---------
+    rho : quti.Qobj
+        Quantum density matrix
+    
+    Returns
+    ---------
+    purity_rho : float
+        The purity of rho(=1 if pure, <1 if not pure)
+    """
+
+    purity_rho=(rho*rho).tr()
+    return purity_rho
